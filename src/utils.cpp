48a29baaa8111d04716a3c536030fa8d11a38be7
GutsIMW <87187490+GutsIMW@users.noreply.github.com>
2024-01-13 20:01:33 +0100

    Added a function to apply a filter on an grayscale image + gaussian filter

--- src/utils.cpp
+++ src/utils.cpp
@@ -81,3 +81,49 @@ LibTIM::Image<LibTIM::U8> sobelFilter(LibTIM::Image<LibTIM::U8> image)
 	}
 	return resImg;
 }
+
+LibTIM::Image<LibTIM::U8> applyFilter(LibTIM::Image<LibTIM::U8> image, std::vector<std::vector<float>> kernel)
+{
+	int dx = image.getSizeX();
+	int dy = image.getSizeY();
+
+	LibTIM::Image<LibTIM::U8> resImg{ static_cast<LibTIM::TSize>(dx), static_cast<LibTIM::TSize>(dy) };
+
+	for (int y = 0; y < dy; y++)
+	{
+		for (int x = 0; x < dx; x++)
+		{
+			int startY = y - int(kernel[0].size() / 2);
+			int startX = x - int(kernel.size() / 2);
+			int endY = y + int(kernel[0].size() / 2);
+			int endX = x + int(kernel.size() / 2);
+
+			/*std::cout << kernel.size() << kernel[0].size() << std::endl;
+			std::cout << kernel.size() / 2 << kernel[0].size() / 2 << std::endl;
+			std::cout << startX << ", " << startY << ", " << endX << ", " << endY << std::endl;*/
+
+			float res = 0;
+			for (int i = startY; i <= endY; i++)
+			{
+				if (i < 0 || i >= dy)
+					continue;
+				for (int j = startX; j <= endX; j++)
+				{
+					if (j < 0 || j >= dx)
+						continue;
+					LibTIM::U8 p = image(j, i);
+					res += kernel[j - startX][i - startY] * p;
+				}
+			}
+			/*res *= */
+			resImg(x, y) = std::clamp(static_cast<int>(res), 0, 255);
+		}
+	}
+	return resImg;
+}
+
+LibTIM::Image<LibTIM::U8> gaussianFilter3x3(LibTIM::Image<LibTIM::U8> image)
+{
+	std::vector<std::vector<float>> gaussian = { {1.f / 16.f, 2.f / 16.f, 1.f / 16.f}, {2.f / 16.f, 4 / 16.f, 2 / 16.f}, {1.f / 16.f, 2.f / 16.f, 1.f / 16.f} };
+	return applyFilter(image, gaussian);
+}
--- src/utils.hpp
+++ src/utils.hpp
@@ -10,3 +10,7 @@ glm::vec3 rgbToCIELAB(LibTIM::RGB pixelRGB);
 
 
 LibTIM::Image<LibTIM::U8> sobelFilter(LibTIM::Image<LibTIM::U8> image);
+
+LibTIM::Image<LibTIM::U8> applyFilter(LibTIM::Image<LibTIM::U8> image, std::vector<std::vector<float>> kernel);
+
+LibTIM::Image<LibTIM::U8> gaussianFilter3x3(LibTIM::Image<LibTIM::U8> image);
