5e075e0a3d720fbdbf0550228187c7679bcfaf73
GutsIMW <87187490+GutsIMW@users.noreply.github.com>
2024-01-13 19:19:20 +0100

    Sobel filter OK

--- src/main.cpp
+++ src/main.cpp
@@ -15,25 +15,32 @@ int main(int argc, char** argv)
 
 	std::cout << std::filesystem::current_path() << std::endl;
 
-	LibTIM::Image<LibTIM::RGB>::load("Images/Peyto_Lake_Panorama.ppm", image);
+	LibTIM::Image<LibTIM::RGB>::load("Images/landscape.ppm", image);
 
 
-	// stocke les dimensions de l'image dans dx et dy
+	//// stocke les dimensions de l'image dans dx et dy
 	int dx = image.getSizeX();
 	int dy = image.getSizeY();
 
-	LibTIM::Image<LibTIM::RGB> imgInCIELAB{static_cast<LibTIM::TSize>(dx), static_cast<LibTIM::TSize>(dy)};
+	//LibTIM::Image<LibTIM::RGB> imgInCIELAB{static_cast<LibTIM::TSize>(dx), static_cast<LibTIM::TSize>(dy)};
+	LibTIM::Image<LibTIM::U8> imgInCIELAB_LONLY{ static_cast<LibTIM::TSize>(dx), static_cast<LibTIM::TSize>(dy) };
+
+
 	for (int y = 0; y < dy; y++)
 		for (int x = 0; x < dx; x++)
 		{
 			glm::vec3 pixelInLAB = rgbToCIELAB(image(x, y)); // Here the value are in [0, 1]
-			imgInCIELAB(x, y) = LibTIM::RGB({
+			/*imgInCIELAB(x, y) = LibTIM::RGB({
 				static_cast<LibTIM::U8>(pixelInLAB.z / 100.f * 255.f), static_cast<LibTIM::U8>(pixelInLAB.z / 100.f * 255.f),
 				static_cast<LibTIM::U8>(pixelInLAB.z / 100.f * 255.f)
-			});
+			});*/
+			imgInCIELAB_LONLY(x, y) = pixelInLAB.x;
 		}
-	imgInCIELAB.save("Images/imgInCIELAB.ppm");
+	//imgInCIELAB.save("Images/imgInCIELAB.ppm");
+	//imgInCIELAB_LONLY.save("Images/imgInCIELAB_LONLY.ppm");
 
+	auto sobelImg = sobelFilter(imgInCIELAB_LONLY);
+	sobelImg.save("Images/imgSobel.ppm");
 
 	auto markers = LibTIM::Image<LibTIM::TLabel>(image.getSizeX(), image.getSizeY());
 
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,7 @@
 #include "utils.hpp"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "Common/Types.h"
 
 glm::vec3 rgbToCIELAB(LibTIM::RGB pixelRGB)
@@ -23,15 +25,59 @@ glm::vec3 rgbToCIELAB(LibTIM::RGB pixelRGB)
 		L = 903.3f * pixelXYZ.y / XYZn.y;
 
 	auto f = [](float t)
-		{
-			if (t > 0.008856)
-				return std::pow(t, 1.f / 3.f);
-			else
-				return 7.7787f * t + 16.f / 116.f;
-		};
+	{
+		if (t > 0.008856)
+			return std::pow(t, 1.f / 3.f);
+		else
+			return 7.7787f * t + 16.f / 116.f;
+	};
 
 	float a = 500.f * (f(pixelXYZ.x / XYZn.x) - f(pixelXYZ.y / XYZn.y));
 	float b = 200.f * (f(pixelXYZ.y / XYZn.y) - f(pixelXYZ.z / XYZn.z));
 
 	return { L, a, b };
+}
+
+LibTIM::Image<LibTIM::U8> sobelFilter(LibTIM::Image<LibTIM::U8> image)
+{
+	int dx = image.getSizeX();
+	int dy = image.getSizeY();
+
+	LibTIM::Image<LibTIM::U8> imgSobelX{ static_cast<LibTIM::TSize>(dx), static_cast<LibTIM::TSize>(dy) };
+	LibTIM::Image<LibTIM::U8> imgSobelY{ static_cast<LibTIM::TSize>(dx), static_cast<LibTIM::TSize>(dy) };
+	LibTIM::Image<LibTIM::U8> resImg{ static_cast<LibTIM::TSize>(dx), static_cast<LibTIM::TSize>(dy) };
+
+	float kernelX[3][3] = { {-1 - 2, -1}, {0, 0, 0}, {1, 2, 1} };
+	float kernelY[3][3] = { {1, 0, -1}, {2, 0, -2}, {1, 0, -1} };
+	for (int y = 0; y < dy; y++)
+	{
+		for (int x = 0; x < dx; x++)
+		{
+			int startY = y - 1;
+			int startX = x - 1;
+			int endY = y + 1;
+			int endX = x + 1;
+
+			float resX = 0.0f;
+			float resY = 0.0f;
+			for (int i = startY; i <= endY; i++)
+			{
+				if (i < 0 || i >= dy)
+					continue;
+				for (int j = startX; j <= endX; j++)
+				{
+					if (j < 0 || j >= dx)
+						continue;
+					float p = image(j, i);
+					resX += kernelX[j - startX][i - startY] * p;
+					resY += kernelY[j - startX][i - startY] * p;
+				}
+			}
+			imgSobelX(x, y) = resX;
+			imgSobelY(x, y) = resY;
+
+			resImg(x, y) = std::sqrt(resX * resX + resY * resY);
+		}
+	}
+	return resImg;
 }
