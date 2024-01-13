c6b40a15e9ebe4bfb441d60af599707d894fd558
PierreEVEN <evenpierre@orange.fr>
2024-01-13 16:13:10 +0100

    fixed glm

--- src/markers.cpp
+++ src/markers.cpp
@@ -1,9 +1,20 @@
 #include "markers.hpp"
 
+#include <glm/glm.hpp>
 
-static std::vector<> makePoints(uint32_t width, uint32_t height, float sigma)
+static std::vector<glm::vec2> makePoints(uint32_t width, uint32_t height, float sigma)
 {
-	
+	std::vector<glm::vec2> points;
+	points.reserve(static_cast<uint32_t>((width / sigma) * (height / sigma)));
+	for (uint32_t x = 0; x < static_cast<uint32_t>(width / sigma); ++x)
+	{
+		for (uint32_t y = 0; y < static_cast<uint32_t>(height / sigma); ++y)
+		{
+			points.emplace_back(glm::vec2{ x * sigma, height * sigma });
+		}		
+	}
+
+	return points;
 }
 
 LibTIM::Image<LibTIM::TLabel> markers(const LibTIM::Image<LibTIM::RGB>& source, float sigma)
@@ -11,6 +22,8 @@ LibTIM::Image<LibTIM::TLabel> markers(const LibTIM::Image<LibTIM::RGB>& source, 
 	auto markers = LibTIM::Image<LibTIM::TLabel>(source.getSizeX(), source.getSizeY());
 	markers.fill(0);
 
+	const auto centers = makePoints(source.getSizeX(), source.getSizeY(), sigma);
 
 
+	return markers;
 }
