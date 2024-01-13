2f2588e731add2b3f8f1b8366a4ca7604e876a4b
PierreEVEN <evenpierre@orange.fr>
2024-01-13 15:42:07 +0100

    added base cod with libtim

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.hpp"
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.hpp"
+#include <cstdint>
+
+#include <libtim/Algorithms/Watershed.h>
+
+#include <filesystem>
 
 int main(int argc, char** argv) {
-	
+	auto image = LibTIM::Image<uint8_t>();
+
+	std::cout << std::filesystem::current_path() << std::endl;
+
+	LibTIM::Image<uint8_t>::load("Images/Peyto_Lake_Panorama.jpg", image);
+
+	auto markers = LibTIM::Image<LibTIM::TLabel>(image);
+
+	LibTIM::FlatSE flatSe;
+
+	//LibTIM::watershedMeyer<uint8_t>(image, markers, flatSe, false);
 }
